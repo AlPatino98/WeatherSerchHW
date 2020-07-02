f8a3354748e0b6fb0500e2a296609d54b779f80e
Aldo Patino <aldopguido@gmail.com>
2020-07-01 23:03:27 -0500

    Added variables I want to see displayed

--- logic.js
+++ logic.js
@@ -5,36 +5,118 @@ var queryTerm = "";
 var startDate = 0;
 var endDate = 0;
 
-// Functions
+// Function to display current weather
 function displayWeather(){
 
 var cityName = $("#search-term").val();
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=austin&appid=25e3a4e6b1b1e4ea16cd382de8c61270";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 
-console.log(queryURL);
+//Ajax call
+$.ajax({
+    url: queryURL,
+    method: "GET" })
+//store all of the retrived data in object "weatherData"
+.then(function(response){
 
+//Element for Variable Name
+ var nameW = (response.name);
+ var wName = $("<h3>").text( nameW)
+
+ //Element for Variable Icon
+ var iconW = (response.weather.icon);
+ var wIcon = $("<img>").attr("src", iconW)
+
+ //Element for Variable Weather
+ var weathW = (response.weather.description);
+ var weather = $("<h5>").text("Condition: "+ weathW)
+
+ //Element for Variable Date
+ var dateW = (response.dt);
+ var wDate = $("<h5>").text("Date: "+ dateW)
+
+//Element for Variable Temp
+ var tempW = (response.main.temp - 273.15) * 1.80 + 32;
+  var pTemp = $("<h5>").text("temp: " + tempW.toFixed(2));
+
+  //Element for Variable Humidity
+ var humW = (response.main.humidity);
+ var wHum = $("<h5>").text("Humidity: "+ humW +"%")
+
+ //Element for Variable Wind Speed
+ var windW = (response.wind.speed);
+ var wWind = $("<h5>").text("Wind Speed: "+ windW +"MPH")
+ 
+
+ $("#weatherResults").append(wName, wIcon, pTemp, weather, wDate, wHum, wWind );
+ 
+});
     
 }
+//function to display Forecast
+function displayForecast(){
+
+    var cityName = $("#search-term").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName +"&appid=" + APIKey;
+    
+    //Ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET" })
+    //store all of the retrived data in object "weatherData"
+    .then(function(response){
+    //  console.log(response);
+    
+ //loop through the span of forecast days
+    for (var i = 0; i < response.list.length; i++) {
+
+        //Element for Variable Name
+ var nameW = (response.name);
+ var wName = $("<h3>").text( nameW)
+
+ //Element for Variable Icon
+ var iconW = (response.list.weather.icon);
+ var wIcon = $("<img>").attr("src", iconW)
+
+ //Element for Variable Weather
+ var weathW = (response.weather.description);
+ var weather = $("<h5>").text("Condition: "+ weathW)
+
+ //Element for Variable Date
+ var dateW = (response.list.dt);
+ var wDate = $("<h5>").text("Date: "+ dateW)
+
+  //Element for Variable Humidity
+ var humW = (response.list.main.humidity);
+ var wHum = $("<h5>").text("Humidity: "+ humW +"%")
+
+ //Element for Variable Wind Speed
+ var windW = (response.list.wind.speed);
+ var wWind = $("<h5>").text("Wind Speed: "+ windW +"MPH")
+
+//Element for variable temp
+ var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+ var pTemp = $("<p>").text("temp: " + tempF.toFixed(2));
+
+        //  console.log(response.list[i]);
+
+         //Append temp to html
+          $("#forecastResults").append(wName, wIcon, pTemp, weather, wDate, wHum, wWind );
+    }
+
+    //  var pTemp = $("<p>").text("temp: ");
+        
+     
+    });
+        
+    }
+
 
 //Run function on click of the search button
 $("#run-search").on("click",function(event){
-      // prevent default
+// prevent default
     event.preventDefault();
-
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=austin&appid=25e3a4e6b1b1e4ea16cd382de8c61270";
-
-//Ajax call
-     $.ajax({
-        url: queryURL,
-        method: "GET"
-    })//store all of the retrived data in object "weatherData"
-    .then(function(response){
-        console.log(response);
   
-        
-});
-   
     // run finction display weather
     displayWeather();
+    displayForecast();
 });
